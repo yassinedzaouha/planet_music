202496c8959e87e680970bf041469670f245a11b
yassinedzaouha <yassinepatin@gmail.com>
2024-03-18 22:03:53 +0000

    update

---
+++ planet.js
@@ -0,0 +1,40 @@
+console.log(parseInt("100 patin"));
+console.log(Number.MAX_VALUE+3000);
+
+console.log(Math.round(99.6));
+
+console.log(Math.round(99.3));
+
+// go to next number
+console.log(Math.ceil(99.2));
+
+// the opposite of ceil 
+console.log(Math.floor(99.9));
+
+console.log(Math.max(10, 20, -10, 200, 30));
+
+console.log(Math.min(10, 20, -10, 200, 30));
+
+console.log(Math.pow(2, 4));
+
+// random number 
+console.log(Math.random());
+
+// remove the Fracture 
+console.log(Math.trunc(99.5)); 
+
+
+
+
+let echiri = ["saif", "patin" , "me" , "mok" , "sof"];
+
+console.log(`wafeen a ${echiri[0]}`);
+console.log(`wafeen a ${echiri[3]}`);
+
+console.log(echiri);
+
+echiri[3] = "amin";
+
+console.log(echiri);
+
+console.log(echiri.length);
